d7650c98ea0dabe4495aa917c5ace22a2d0de174
Sean Tighe <seanpatricktighe@gmail.com>
2024-10-11 14:00:55 +0100

    all

--- new-game-project/node_3d.gd
+++ new-game-project/node_3d.gd
@@ -8,10 +8,11 @@ extends Node3D
 func _ready() -> void:
 	for row in range(rows):
 		for col in range(cols):
-			var brick = brick_scene.intstantiate()
+			var brick = brick_scene.instantiate()
 			var pos = Vector3(col, row, 0)
 			brick.position = pos
 			add_child(brick)
+		
 	pass # Replace with function body.
 
 
