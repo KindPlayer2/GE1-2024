3e8db734e106d6e3cde2fe9f54b3504bf304e1c5
Sean Tighe <seanpatricktighe@gmail.com>
2024-10-25 16:04:17 +0100

    commit 2

--- games-engines-2024/scenes/enemy.gd
+++ games-engines-2024/scenes/enemy.gd
@@ -2,15 +2,13 @@ extends MeshInstance3D
 
 @onready var tank:Node = $"../tank"
 
+var q1:Quaternion
+var q2:Quaternion
+
+var t = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	
 	var to_player = tank.global_position - global_position
 	to_player = to_player.normalized()
@@ -24,8 +22,26 @@ func _process(delta: float) -> void:
 	
 	var theta = acos(to_player.dot(forw))
 	
-	DebugDraw2D.set_text("angle to player", rad_to_deg(theta))
+	q2 = Quaternion(axis, theta)
+	q1 = global_basis.get_rotation_quaternion()
+	t=0
 	
-	rotation = Vector3(0, - theta, 0)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var q:Quaternion
+	
+	
+	#commit 
+	if t < 1.0:
+		t=t + delta
+	else:
+		t =t + 1
+		
+	q = q1.slerp(q2, t)
+	global_basis = Basis(q)
 	
 	pass
